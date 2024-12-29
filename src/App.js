ce27b230942b7c7ed54d8f71a5055748fc67bb96
AhmedSultan2002 <ahmedsultanx2002@gmail.com>
2024-12-29 22:34:47 +0500

    widget integrated

--- src/App.js
+++ src/App.js
@@ -11,21 +11,22 @@ import TutorialsPage from './pages/TutorialsPage';
 import MentorsPage from './pages/MentorsPage';
 
 function App() {
-    return (
-        <Router>
-            <Header />
-            <Routes>
-                <Route path="/" element={<Homepage />} />
-                <Route path="/shop" element={<ShopPage />} />
-                <Route path="/login" element={<LoginPage />} />
-                <Route path="/register" element={<RegisterPage />} />
-                <Route path="/story/:productId" element={<StoryPage />} />
-                <Route path="/tutorials" element={<TutorialsPage />} />
-                <Route path="/mentors" element={<MentorsPage />} />
-            </Routes>
-            <Footer />
-        </Router>
-    );
+
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/shop" element={<ShopPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/story/:productId" element={<StoryPage />} />
+        <Route path="/tutorials" element={<TutorialsPage />} />
+        <Route path="/mentors" element={<MentorsPage />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
 }
 
 export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,45 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import '../css/Header.css';
+  import React, { useEffect } from 'react';
+  import { Link } from 'react-router-dom';
+  import '../css/Header.css';
 
-function Header() {
-  return (
-    <header className="header">
-      <nav className="navbar">
-        <div className="logo">The Bazaar</div>
-        <ul className="nav-links">
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/shop">Shop</Link></li>
-          <li><Link to="/mentors">Mentors</Link></li>
-          <li><Link to="/tutorials">Tutorials</Link></li>
-        </ul>
-      </nav>
-    </header>
-  );
-}
+  function Header() {
+    useEffect(() => {
+      // Check if the script is already loaded to avoid duplication
+      if (!document.getElementById('google-translate-script')) {
+        const script = document.createElement('script');
+        script.src = 'https://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit';
+        script.id = 'google-translate-script'; // Add an ID to the script tag to avoid re-adding it
+        script.async = true;
+        document.body.appendChild(script);
+      }
 
-export default Header;
+      // Initialize the Google Translate widget once the script is loaded
+      window.googleTranslateElementInit = () => {
+        new window.google.translate.TranslateElement(
+          { pageLanguage: 'en' },  // Default page language (can be changed)
+          'google_translate_element'  // Element ID where the widget will be rendered
+        );
+      };
+    }, []);
+
+    return (
+      <header className="header">
+        <nav className="navbar">
+          <div className="logo">The Bazaar</div>
+          <ul className="nav-links">
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/shop">Shop</Link></li>
+            <li><Link to="/mentors">Mentors</Link></li>
+            <li><Link to="/tutorials">Tutorials</Link></li>
+          </ul>
+          {/* Google Translate Widget */}
+          <div
+            id="google_translate_element"
+            style={{ marginLeft: '20px', marginTop: '8px' }}
+          ></div>
+        </nav>
+      </header>
+    );
+  }
+
+  export default Header;
